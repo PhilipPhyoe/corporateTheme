c883825d4849da093473e45ae61ca6e4a3fb8b35
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-01-30 21:37:23 +0630

    add footer.jsx and footer.css and modify layout and info.css to add footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelopeOpenText,
+  faLocationArrow,
+} from "@fortawesome/free-solid-svg-icons";
+import Info from "./Info";
+import "../assets/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>
+        <img src="/images/main logo.png" className="footer-logo" />
+      </div>
+      <div className="address">
+        <h2 className="footer-company">Hector Myanmar Group</h2>
+        <div className="street">
+          <FontAwesomeIcon icon={faLocationArrow} className="icon1" />
+          <p>
+            No.920/921, Bayint Naung road, 43 ward, <br />
+            North Dagon Township, Yangon., Yangon, Myanmar
+          </p>
+        </div>
+        <div className="footer-email">
+          <FontAwesomeIcon icon={faEnvelopeOpenText} className="icon2" />
+          <p>info@hectormyanmar.com</p>
+        </div>
+        <Info />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
