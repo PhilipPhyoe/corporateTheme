204388951275be84ef3b7f8455c4450d5618e9a8
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-01-31 15:52:22 +0630

    design hero section

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -3,16 +3,18 @@ import "../assets/Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
-      <h1 className="hero-title">
-        Reliable <span style={{ color: "var(--primary)" }}>Corporate</span>
-        <br />
-        Rental Services
-      </h1>
-      <p className="hero-text">
-        Our corporate car rental services ensure
-        <br />
-        safety, comfort and punctuality.
-      </p>
+      <div className="hero-content">
+        <h1 className="hero-title">
+          Reliable <span style={{ color: "orange" }}>Corporate</span>
+          <br />
+          Rental Services
+        </h1>
+        <p className="hero-text">
+          Our corporate car rental services ensure
+          <br />
+          safety, comfort and punctuality.
+        </p>
+      </div>
     </div>
   );
 };
