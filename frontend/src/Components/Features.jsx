39e5aa60f90266af7681bfcb7348d415ab2f8043
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-02-01 12:07:16 +0630

    modify features jsx and css for hover effects

--- frontend/src/Components/Features.jsx
+++ frontend/src/Components/Features.jsx
@@ -30,28 +30,34 @@ const Features = ({
         </p>
       </div>
       <div className="feature-cards">
-        <div className="card1">
-          <FontAwesomeIcon icon={icon1} className="card-icon" />
-          <h4 className="card-title">{card1}</h4>
-          <p className="card-text">{text1}</p>
+        <div className="card-hover1">
+          <div className="card1">
+            <FontAwesomeIcon icon={icon1} className="card-icon" />
+            <h4 className="card-title">{card1}</h4>
+            <p className="card-text">{text1}</p>
+          </div>
           <Link to="/services" className="link1">
             <p className="link-text">Learn More</p>
             <FontAwesomeIcon icon={faArrowRight} className="link-icon" />
           </Link>
         </div>
-        <div className="card2">
-          <FontAwesomeIcon icon={icon2} className="card-icon" />
-          <h4 className="card-title">{card2}</h4>
-          <p className="card-text">{text2}</p>
+        <div className="card-hover2">
+          <div className="card2">
+            <FontAwesomeIcon icon={icon2} className="card-icon" />
+            <h4 className="card-title">{card2}</h4>
+            <p className="card-text">{text2}</p>
+          </div>
           <Link to="/services" className="link2">
             <p className="link-text">Learn More</p>
             <FontAwesomeIcon icon={faArrowRight} className="link-icon" />
           </Link>
         </div>
-        <div className="card3">
-          <FontAwesomeIcon icon={icon3} className="card-icon" />
-          <h4 className="card-title">{card3}</h4>
-          <p className="card-text">{text3}</p>
+        <div className="card-hover3">
+          <div className="card3">
+            <FontAwesomeIcon icon={icon3} className="card-icon" />
+            <h4 className="card-title">{card3}</h4>
+            <p className="card-text">{text3}</p>
+          </div>
           <Link to="/services" className="link3">
             <p className="link-text">Learn More</p>
             <FontAwesomeIcon icon={faArrowRight} className="link-icon" />
