41c5167ecdbe7f7c746b67b1395d97e19cc28f77
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-02-03 20:46:25 +0630

    change the website to become responsive, changing css files

--- frontend/src/Components/Menu.jsx
+++ frontend/src/Components/Menu.jsx
@@ -1,33 +1,74 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../assets/Menu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Menu = () => {
+  const [mobile, setMobile] = useState(false);
+  const showMobileMenu = () => {
+    setMobile(!mobile);
+  };
   return (
-    <div className="menu-bar">
-      <NavLink
-        to="/"
-        className={(navData) => (navData.isActive ? "active" : "menu-item")}
+    <div>
+      <div className="menu-bar">
+        <NavLink
+          to="/"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          About Us
+        </NavLink>
+        <NavLink
+          to="/services"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          Services
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          Contact Us
+        </NavLink>
+      </div>
+      <div className="small-menu">
+        <FontAwesomeIcon icon={faBars} onClick={showMobileMenu} />
+      </div>
+      <div
+        className={`mobile-menu ${mobile ? "show" : ""}`}
+        onClick={showMobileMenu}
       >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        className={(navData) => (navData.isActive ? "active" : "menu-item")}
-      >
-        About Us
-      </NavLink>
-      <NavLink
-        to="/services"
-        className={(navData) => (navData.isActive ? "active" : "menu-item")}
-      >
-        Services
-      </NavLink>
-      <NavLink
-        to="/contact"
-        className={(navData) => (navData.isActive ? "active" : "menu-item")}
-      >
-        Contact Us
-      </NavLink>
+        <NavLink
+          to="/"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          About Us
+        </NavLink>
+        <NavLink
+          to="/services"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          Services
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={(navData) => (navData.isActive ? "active" : "menu-item")}
+        >
+          Contact Us
+        </NavLink>
+      </div>
     </div>
   );
 };
